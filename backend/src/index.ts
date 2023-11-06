32cba2557b5f4d1a795b1cfa28bb23f22835f0b5
katty-bird <s05777733@htw-berlin.de>
2023-11-06 17:13:31 +0100

    Revert "Update MongoDB connection string and add Prisma"
    This reverts commit bb55a82368ae82f969c6a684fcebb7e1b972618b.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,49 +1,11 @@
-import { PrismaClient } from '@prisma/client'
 import express from 'express'
-import cors from 'cors'
 const app = express()
-const port = 4000
-
-const prisma = new PrismaClient()
-
-async function main() {
-    await prisma.user.create({
-        data: {
-          name: 'Rich',
-          email: 'hello@prisma.com',
-          sports: {
-            create: {
-              name: 'Football',
-              location: 'TU Berlin',
-              slug: 'football-tu',
-            },
-          },
-        },
-      })
-    
-      const allUsers = await prisma.user.findMany({
-        include: {
-          sports: true,
-        },
-      })
-      console.dir(allUsers, { depth: null })
-}
+const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  
+  res.send('Hello World!')
+})
+
 app.listen(port, () => {
-    console.log(`Express is listening at http://localhost:${port}`)
-  })
-
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
-
+  console.log(`Express is listening at http://localhost:${port}`)
+})

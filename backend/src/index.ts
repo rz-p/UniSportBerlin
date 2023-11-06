bb55a82368ae82f969c6a684fcebb7e1b972618b
rz-p <pramadhan9@gmail.com>
2023-11-06 15:49:58 +0100

    Update MongoDB connection string and add Prisma client and types

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,49 @@
+import { PrismaClient } from '@prisma/client'
 import express from 'express'
+import cors from 'cors'
 const app = express()
-const port = 3000
+const port = 4000
+
+const prisma = new PrismaClient()
+
+async function main() {
+    await prisma.user.create({
+        data: {
+          name: 'Rich',
+          email: 'hello@prisma.com',
+          sports: {
+            create: {
+              name: 'Football',
+              location: 'TU Berlin',
+              slug: 'football-tu',
+            },
+          },
+        },
+      })
+    
+      const allUsers = await prisma.user.findMany({
+        include: {
+          sports: true,
+        },
+      })
+      console.dir(allUsers, { depth: null })
+}
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
+    res.send('Hello World!')
+  })
+  
 app.listen(port, () => {
-  console.log(`Express is listening at http://localhost:${port}`)
-})
+    console.log(`Express is listening at http://localhost:${port}`)
+  })
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
+

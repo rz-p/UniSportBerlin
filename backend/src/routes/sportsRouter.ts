3e0e32dfb4c38b6cf7ddd4ce631fbddef099de50
rz-p <pramadhan9@gmail.com>
2023-12-03 02:49:48 +0100

    Add likeSport route and update HomePage component

--- backend/src/routes/sportsRouter.ts
+++ backend/src/routes/sportsRouter.ts
@@ -2,10 +2,12 @@ import express from 'express'
 import findAllSports from './sport/findAllSport'
 import createSport from './sport/createSport'
 import deleteSport from './sport/deleteSport'
+import likeSport from './sport/likeSport'
 
 const sportRouter = express.Router()
 
 sportRouter.use('/all', findAllSports)
+sportRouter.use('/:slug/like', likeSport)
 sportRouter.use('/new', createSport)
 sportRouter.use('/delete', deleteSport)
 
--- frontend/client/src/pages/HomePage.tsx
+++ frontend/client/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
@@ -8,41 +8,78 @@ import Checkbox from "@mui/material/Checkbox";
 import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
 import Favorite from "@mui/icons-material/Favorite";
 import { Sport } from "../common/types";
-import getAllSports from "../actions/SportActions";
+import { getAllSports, addLike, dislike } from "../actions/SportActions";
 
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 export default function HomePage() {
-    const [likes, setLikes] = React.useState(0);
-    const data = getAllSports();
+  const [likes, setLikes] = useState(0);
+  const [sports, setSports] = useState<Sport[]>([]);
 
-    return (
-        <div
-            style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
-        >
-            {Object.values(data).map((sport) => (
-                <Card key={sport.slug} sx={{ maxWidth: 345, margin: "10px" }}>
-                    <CardActionArea>
-                        <CardMedia sx={{ height: 250 }} image={sport.image} />
-                        <CardContent sx={{ height: 150 }}>
-                            <Typography gutterBottom variant="h5" component="div">
-                                {sport.name}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                {sport.details}
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                    <CardActions disableSpacing>
-                        <Checkbox
-                            {...label}
-                            icon={<FavoriteBorder />}
-                            checkedIcon={<Favorite />}
-                            onChange={() => setLikes(likes + 1)}
-                        />
-                    </CardActions>
-                </Card>
-            ))}
-        </div>
-    )
+  useEffect(() => {
+    const fetchSports = async () => {
+      try {
+        const sportsData = await getAllSports();
+        setSports(sportsData);
+      } catch (error) {
+        console.error("Error fetching sports:", error);
+      }
+    };
+
+    fetchSports();
+  }, []);
+
+  return (
+    <div
+      style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+    >
+      {sports.map((sport) => (
+        <Card key={sport.slug} sx={{ maxWidth: 345, margin: "10px" }}>
+          <CardActionArea>
+            {sport.image ? (
+              <CardMedia sx={{ height: 250 }} image={sport.image} />
+            ) : (
+              <div
+                style={{
+                  height: 250,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                Image Not Available
+              </div>
+            )}
+            <CardContent sx={{ height: 150 }}>
+              <Typography gutterBottom variant="h5" component="div">
+                {sport.name}
+              </Typography>
+              <CardContent sx={{ height: 10 }}>
+                <Typography variant="subtitle2" color="text.secondary">
+                  {sport.location}
+                </Typography>
+                <Typography variant="subtitle2" color="text.secondary">
+                  {sport.schedule}
+                </Typography>
+              </CardContent>
+              <Typography variant="body2" color="text.secondary">
+                {sport.details}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions disableSpacing>
+            <Checkbox
+              {...label}
+              icon={<FavoriteBorder />}
+              checkedIcon={<Favorite />}
+              onChange={() => setLikes(likes + 1)}
+            />
+            <Typography variant="body2" color="text.secondary">
+              {likes}
+            </Typography>
+          </CardActions>
+        </Card>
+      ))}
+    </div>
+  );
 }

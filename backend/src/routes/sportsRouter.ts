2e314b346d6c7dfd68b4d709afd121e7efbb897b
rz-p <pramadhan9@gmail.com>
2023-12-27 15:51:59 +0100

    Add updateSport route to sportsRouter

--- backend/src/routes/sportsRouter.ts
+++ backend/src/routes/sportsRouter.ts
@@ -3,6 +3,7 @@ import findAllSports from './sport/findAllSport'
 import createSport from './sport/createSport'
 import deleteSport from './sport/deleteSport'
 import likeSport from './sport/likeSport'
+import updateSport from './sport/updateSport'
 
 const sportRouter = express.Router()
 
@@ -10,5 +11,6 @@ sportRouter.use('/all', findAllSports)
 sportRouter.use('/:slug/like', likeSport)
 sportRouter.use('/new', createSport)
 sportRouter.use('/delete', deleteSport)
+sportRouter.use('/:slug/update', updateSport)
 
 export default sportRouter;
